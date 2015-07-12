ad93ea2d75dce65bbc7d9220979acd6d15caf35b
Corey Bryant <corey.bryant@canonical.com>
2015-07-12 21:03:26 +0000

    Fixup amulet git repos

--- tests/basic_deployment.py
+++ tests/basic_deployment.py
@@ -64,13 +64,19 @@ class KeystoneBasicDeployment(OpenStackAmuletDeployment):
         if self.git:
             branch = 'stable/' + self._get_openstack_release_string()
             amulet_http_proxy = os.environ.get('AMULET_HTTP_PROXY')
+            if self._get_openstack_release() == self.trusty_icehouse:
+                reqs_repo = 'git://github.com/coreycb/requirements'
+                keystone_repo = 'git://github.com/coreycb/keystone'
+            else:
+                reqs_repo = 'git://github.com/openstack/requirements'
+                keystone_repo = 'git://github.com/openstack/keystone'
             openstack_origin_git = {
                 'repositories': [
                     {'name': 'requirements',
-                     'repository': 'git://github.com/openstack/requirements',
+                     'repository': reqs_repo,
                      'branch': branch},
                     {'name': 'keystone',
-                     'repository': 'git://github.com/coreycb/keystone',
+                     'repository': keystone_repo,
                      'branch': branch},
                 ],
                 'directory': '/mnt/openstack-git',
