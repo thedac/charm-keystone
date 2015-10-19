38c97f6b47478d46e8bd8a3449cc65b575fffc26
David Della Vecchia <ddv@canonical.com>
2015-10-19 20:52:06 +0000

    reordering function calls to avoid race condition

--- hooks/keystone_hooks.py
+++ hooks/keystone_hooks.py
@@ -121,16 +121,15 @@ def install():
     status_set('maintenance', 'Executing pre-install')
     execd_preinstall()
     configure_installation_source(config('openstack-origin'))
-
-    unison.ensure_user(user=SSH_USER, group='juju_keystone')
-    unison.ensure_user(user=SSH_USER, group='keystone')
-
     status_set('maintenance', 'Installing apt packages')
     apt_update()
     apt_install(determine_packages(), fatal=True)
 
     status_set('maintenance', 'Git install')
     git_install(config('openstack-origin-git'))
+
+    unison.ensure_user(user=SSH_USER, group='juju_keystone')
+    unison.ensure_user(user=SSH_USER, group='keystone')
 
 
 @hooks.hook('config-changed')
