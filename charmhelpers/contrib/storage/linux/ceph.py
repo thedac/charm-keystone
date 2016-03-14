7a5ad0ce046e422f31aa6d80170a313e083501ee
Liam Young <liam.young@canonical.com>
2016-03-14 09:55:35 +0000

    Retry Connection to Manager and charmhelper sync
    There is a small window between the keystone init script exiting on startup and
    the keystone api endpoint responding to requets. This change adds the
    retry_on_exception decorator to retry the connection to workaround this.
    
    Change-Id: I9762e195bed042dd201a251252e4214192cad136

--- charmhelpers/contrib/storage/linux/ceph.py
+++ charmhelpers/contrib/storage/linux/ceph.py
@@ -173,7 +173,7 @@ class Pool(object):
         elif mode == 'writeback':
             check_call(['ceph', '--id', self.service, 'osd', 'tier', 'cache-mode', cache_pool, 'forward'])
             # Flush the cache and wait for it to return
-            check_call(['ceph', '--id', self.service, '-p', cache_pool, 'cache-flush-evict-all'])
+            check_call(['rados', '--id', self.service, '-p', cache_pool, 'cache-flush-evict-all'])
             check_call(['ceph', '--id', self.service, 'osd', 'tier', 'remove-overlay', self.name])
             check_call(['ceph', '--id', self.service, 'osd', 'tier', 'remove', self.name, cache_pool])
 
@@ -271,7 +271,7 @@ def get_mon_map(service):
     try:
         mon_status = check_output(
             ['ceph', '--id', service,
-             'ceph', 'mon_status', '--format=json'])
+             'mon_status', '--format=json'])
         try:
             return json.loads(mon_status)
         except ValueError as v:
@@ -321,7 +321,7 @@ def monitor_key_delete(service, key):
     try:
         check_output(
             ['ceph', '--id', service,
-             'ceph', 'config-key', 'del', str(key)])
+             'config-key', 'del', str(key)])
     except CalledProcessError as e:
         log("Monitor config-key put failed with message: {}".format(
             e.output))
@@ -339,7 +339,7 @@ def monitor_key_set(service, key, value):
     try:
         check_output(
             ['ceph', '--id', service,
-             'ceph', 'config-key', 'put', str(key), str(value)])
+             'config-key', 'put', str(key), str(value)])
     except CalledProcessError as e:
         log("Monitor config-key put failed with message: {}".format(
             e.output))
@@ -356,7 +356,7 @@ def monitor_key_get(service, key):
     try:
         output = check_output(
             ['ceph', '--id', service,
-             'ceph', 'config-key', 'get', str(key)])
+             'config-key', 'get', str(key)])
         return output
     except CalledProcessError as e:
         log("Monitor config-key get failed with message: {}".format(
